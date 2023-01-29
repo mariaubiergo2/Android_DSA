2f48216573ebacd4d5463ebf48b662644a6d3db6
Maria <maria.ubiergo@estudiantat.upc.edu>
2023-01-29 13:09:34 +0100

    calendari register

--- app/src/main/java/edu/upc/dsa/andoroid_dsa/activities/RegisterActivity.java
+++ app/src/main/java/edu/upc/dsa/andoroid_dsa/activities/RegisterActivity.java
@@ -1,13 +1,18 @@
 package edu.upc.dsa.andoroid_dsa.activities;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Calendar;
 
 import edu.upc.dsa.andoroid_dsa.Api;
 import edu.upc.dsa.andoroid_dsa.R;
@@ -21,9 +26,10 @@ public class RegisterActivity extends AppCompatActivity {
 
     TextInputEditText nameTxt;
     TextInputEditText surnameTxt;
-    TextInputEditText birthdayTxt;
+    EditText birthdayTxt;
     TextInputEditText emailTxt;
     TextInputEditText passwordRegisterTxt;
+    DatePickerDialog picker;
 
     Api APIservice;
 
@@ -32,12 +38,31 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_main);
 
+
+        birthdayTxt = findViewById(R.id.birthdayTxt);
+        birthdayTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                picker = new DatePickerDialog(RegisterActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                        birthdayTxt.setText(dayOfMonth+"/"+month+"/"+year);
+                    }
+                },year, month, day);
+                picker.show();
+            }
+        });
+
     }
 
     public void doRegister(View view){
         nameTxt = findViewById(R.id.nameTxt);
         surnameTxt = findViewById(R.id.surnameTxt);
-        birthdayTxt = findViewById(R.id.birthdayTxt);
         emailTxt = findViewById(R.id.emailTxt);
         passwordRegisterTxt = findViewById(R.id.passwordRegisterTxt);
         String defaultProfilePicture="https://i.pinimg.com/236x/e9/57/2a/e9572a70726980ed5445c02e1058760b.jpg";
